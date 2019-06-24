e8048a88d662540a025d7f15759cee0730cdbcda
Himani Agrawal <himani93@gmail.com>
2019-06-24 19:43:41 +0530

    Create a libvirt machine

--- pkg/cloud/helloworld/actuators/machine/actuator.go
+++ pkg/cloud/helloworld/actuators/machine/actuator.go
@@ -2,7 +2,11 @@ package machine
 
 import (
 	"context"
+	"fmt"
+	"log"
 
+	libvirt "github.com/libvirt/libvirt-go"
+	libvirtxml "github.com/libvirt/libvirt-go-xml"
 	clusterv1 "sigs.k8s.io/cluster-api/pkg/apis/cluster/v1alpha1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -23,7 +27,109 @@ type Actuator struct {
 }
 
 func (a *Actuator) Create(ctx context.Context, cluster *clusterv1.Cluster, machine *clusterv1.Machine) error {
-	panic("not implemented")
+	log.Printf("Create machine actuator called for context %v, cluster %v, machine %v", ctx, cluster, machine)
+	domcfg := &libvirtxml.Domain{}
+	xmldoc, err := domcfg.Marshal()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("XML: %s\n", xmldoc)
+	bootOrder := uint(1)
+	domcfg = &libvirtxml.Domain{
+		Type: "qemu",
+		Name: "Eddie10",
+		Memory: &libvirtxml.DomainMemory{
+			Value: 4,
+			Unit:  "GB",
+		},
+		VCPU: &libvirtxml.DomainVCPU{
+			Value: 2,
+		},
+		OS: &libvirtxml.DomainOS{
+			Type: &libvirtxml.DomainOSType{
+				Type: "hvm",
+			},
+		},
+		Devices: &libvirtxml.DomainDeviceList{
+			Emulator: "/usr/bin/kvm-spice",
+			Disks: []libvirtxml.DomainDisk{
+				libvirtxml.DomainDisk{
+					Device: "disk",
+					Source: &libvirtxml.DomainDiskSource{
+						File: &libvirtxml.DomainDiskSourceFile{
+							File: "/home/whitebyte/libvirt-experiments/images/bionic-10.qcow2",
+						},
+					},
+					Target: &libvirtxml.DomainDiskTarget{
+						Dev: "hda",
+					},
+					Boot: &libvirtxml.DomainDeviceBoot{
+						Order: bootOrder,
+					},
+					Driver: &libvirtxml.DomainDiskDriver{
+						Name: "qemu",
+						Type: "qcow2",
+					},
+				},
+				libvirtxml.DomainDisk{
+					Device: "cdrom",
+					Source: &libvirtxml.DomainDiskSource{
+						File: &libvirtxml.DomainDiskSourceFile{
+							File: "/home/whitebyte/libvirt-experiments/images/user-data.img",
+						},
+					},
+					Target: &libvirtxml.DomainDiskTarget{
+						Dev: "hdb",
+					},
+				},
+			},
+			Serials: []libvirtxml.DomainSerial{
+				libvirtxml.DomainSerial{
+					Protocol: &libvirtxml.DomainChardevProtocol{
+						Type: "serial",
+					},
+					Target: &libvirtxml.DomainSerialTarget{
+						Port: new(uint),
+					},
+				},
+			},
+			Consoles: []libvirtxml.DomainConsole{
+				libvirtxml.DomainConsole{
+					Target: &libvirtxml.DomainConsoleTarget{
+						Type: "serial",
+						Port: new(uint),
+					},
+				},
+				libvirtxml.DomainConsole{
+					Target: &libvirtxml.DomainConsoleTarget{
+						Type: "virtio",
+					},
+				},
+			},
+			Graphics: []libvirtxml.DomainGraphic{
+				libvirtxml.DomainGraphic{
+					Spice: &libvirtxml.DomainGraphicSpice{
+						AutoPort: "yes",
+					},
+				},
+			},
+		},
+	}
+	xmldoc, err = domcfg.Marshal()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("XML: %s", xmldoc)
+
+	conn, err := libvirt.NewConnect("qemu:///system")
+	if err != nil {
+		panic(err)
+	}
+	_, err = conn.DomainCreateXML(xmldoc, 0)
+	if err != nil {
+		panic(err)
+	}
+	return nil
 }
 
 func (a *Actuator) Delete(ctx context.Context, cluster *clusterv1.Cluster, machine *clusterv1.Machine) error {
