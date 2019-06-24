28956e12975e6013ea70a9c7b4cb419e66246375
Himani Agrawal <himani93@gmail.com>
2019-06-24 12:33:13 +0530

    Add machine actuator skeleton

--- pkg/cloud/helloworld/actuators/machine/actuator.go
+++ pkg/cloud/helloworld/actuators/machine/actuator.go
@@ -1,6 +1,11 @@
 package machine
 
-import "sigs.k8s.io/controller-runtime/pkg/client"
+import (
+	"context"
+
+	clusterv1 "sigs.k8s.io/cluster-api/pkg/apis/cluster/v1alpha1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
 
 const (
 	ProviderName = "helloworld"
@@ -15,6 +20,30 @@ const (
 // Actuator is responsible for performing machine reconciliation
 type Actuator struct {
 	client client.Client
+}
+
+func (a *Actuator) Create(ctx context.Context, cluster *clusterv1.Cluster, machine *clusterv1.Machine) error {
+	panic("not implemented")
+}
+
+func (a *Actuator) Delete(ctx context.Context, cluster *clusterv1.Cluster, machine *clusterv1.Machine) error {
+	panic("not implemented")
+}
+
+func (a *Actuator) Update(ctx context.Context, cluster *clusterv1.Cluster, machine *clusterv1.Machine) error {
+	panic("not implemented")
+}
+
+func (a *Actuator) Exists(ctx context.Context, cluster *clusterv1.Cluster, machine *clusterv1.Machine) (bool, error) {
+	panic("not implemented")
+}
+
+func (a *Actuator) GetIP(cluster *clusterv1.Cluster, machine *clusterv1.Machine) (string, error) {
+	panic("not implemented")
+}
+
+func (a *Actuator) GetKubeConfig(cluster *clusterv1.Cluster, machine *clusterv1.Machine) (string, error) {
+	panic("not implemented")
 }
 
 type ActuatorParams struct {
